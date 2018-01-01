60b143913d4a30a771cb4069f24c1fd05481cfae
dprajpati <dprajapati@officebrain.com>
2018-01-01 20:28:37 +0530

    first commit

---
+++ app/Libraries/AdminUserProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Libraries;
+use Illuminate\Auth\EloquentUserProvider;
+class AdminUserProvider extends EloquentUserProvider
+{
+	public function retrieveByCredentials(array $credentials)
+	{
+		$user = parent::retrieveByCredentials($credentials);
+		if($user === null || !$user->is_admin)
+		{
+			return null;
+		}
+		return $user;
+	}
+}
